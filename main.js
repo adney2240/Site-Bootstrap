23adf4a1062a97a1f040d03ae9ed0d01b8ec7719
adney <48107171+adney2240@users.noreply.github.com>
2022-05-13 12:11:40 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+function escolha (){
+    var escolher = document.getElementById("gameboy").innerHTML
+    var troca = document.getElementById("emulador").innerHTML
+  
+    
+    
+    if(escolher = "Game Boy"){
+        
+      troca = "Game Boy"
+        
+        
+        
+
+    }
+
+    
+}
